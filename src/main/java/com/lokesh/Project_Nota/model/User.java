e78b668e88a4a9c36544f7299419b0fb209d4d06
LokeshSiddi <siddilokesh06@gmail.com>
2025-09-05 22:21:21 +0530

    connecting to Keycloak v2

--- src/main/java/com/lokesh/Project_Nota/model/User.java
+++ src/main/java/com/lokesh/Project_Nota/model/User.java
@@ -1,6 +1,7 @@
 package com.lokesh.Project_Nota.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class User {
 
+    @Id
     private String id;
     private String username;
     private String email;
